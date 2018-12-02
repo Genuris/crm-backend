b697a5a1f31e5c79736aa8d0eb9764c9ae29ecf2
pm_225 <ka@lindenvalley.de>
2018-12-02 17:27:33 +0200

    rest api functional, GET check-api method

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use Illuminate\Routing\Controller as BaseController;
+use Symfony\Component\HttpFoundation\Request;
+
+class ApiController extends BaseController
+{
+    public function __construct()
+    {
+        $this->middleware(function ($request, $next) {
+
+            /*$authorization_bearer = $request->header('Authorization');
+            $authorization_token = 'Bearer '. env('API_TOKEN');
+
+            if (is_null($authorization_bearer) || $authorization_token != $authorization_bearer) {
+                return response()->json(array('error' => array('status' => 401, 'message' => 'Unauthorized. The user needs to be authenticated.')), 401);
+            }*/
+            return $next($request);
+        });
+    }
+
+
+    public function checkApi(Request $request) {
+        /*$token = $request->get('token');
+        if (is_null($token)) {
+            return response()->json(array('error' => array('status' => 400, 'message' => 'body error')), 400);
+        }
+        $user = User::where('api_key', '=', $token)->first();
+        if (!$user) {
+            return response()->json(array('error' => array('status' => 400, 'message' => 'user not found')), 400);
+        }*/
+        return response()->json(array('data' => array(), 'status' => 200), 200);
+    }
+
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::group(['namespace' => 'Api'], function () {
+    Route::get('check-api', 'ApiController@checkApi');
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
