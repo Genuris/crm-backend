adbbd4bdd95d92221252f026f967d71a41637f78
pm_225 <ka@lindenvalley.de>
2019-01-10 20:47:59 +0200

    fix cors data

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     {
         header('Access-Control-Allow-Origin:  *');
         header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
-        header('Access-Control-Allow-Methods:  POST, PUT, GET, DELETE');
+        header('Access-Control-Allow-Methods: *');
         return $next($request);
     }
 }
