bb2db37c1caa901fa09beefea8927cf9961d1f48
pm_225 <ka@lindenvalley.de>
2019-01-10 21:12:35 +0200

    change scrf

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,9 +15,9 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        header('Access-Control-Allow-Origin:  *');
+        /*header('Access-Control-Allow-Origin:  *');
         header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
-        header('Access-Control-Allow-Methods: *');
+        header('Access-Control-Allow-Methods: *');*/
         return $next($request);
     }
 }
