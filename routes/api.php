37a29acf54e292c7985f50cd8096ca7e48a0dcab
pm_225 <ka@lindenvalley.de>
2019-01-13 13:59:24 +0200

    remove dd

--- routes/api.php
+++ routes/api.php
@@ -72,8 +72,6 @@ Route::post('offices', 'ApiOfficesController@store');
 Route::put('offices', 'ApiOfficesController@update');
 Route::delete('offices/{id}', 'ApiOfficesController@delete');
 
-//dd('hello');
-
 //cards
 Route::get('cards', 'ApiCardsController@index');
 Route::get('cards/{id}', 'ApiCardsController@show');
