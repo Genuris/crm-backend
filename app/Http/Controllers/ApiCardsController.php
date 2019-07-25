c917c170e3989128294c84c14c9d660b994e9671
pm_225 <ka@lindenvalley.de>
2019-07-25 22:07:39 +0300

    fix

--- app/Http/Controllers/ApiCardsController.php
+++ app/Http/Controllers/ApiCardsController.php
@@ -616,16 +616,9 @@ class ApiCardsController extends Controller
         if (!is_null($card->area)) {
             $areas = explode(",", $card->area);
             if (is_array($areas) && !empty($areas) && count($areas) > 1) {
-                $query->where(function($q, $areas){
-                    $q->orWhereNull('area')
-                        ->whereIn('area', $areas);
-                });
+                $query->whereIn('area', $areas);
             } else {
-                $query->where(function($q, $card){
-                    $q->orWhereNull('area')
-                        ->y->where('area', 'like', '%'.$card->area.'%');
-                });
-                //$query->where('area', 'like', '%'.$card->area.'%');
+                $query->where('area', 'like', '%'.$card->area.'%');
             }
         } else {
             $query->whereNull('area');
