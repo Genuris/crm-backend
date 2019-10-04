4a35d5baa30f952fe5ad143df35250efad5e5347
pm_225 <ka@lindenvalley.de>
2019-10-04 17:29:42 +0300

    add new models

--- app/Models/CardRequestPosts.php
+++ app/Models/CardRequestPosts.php
@@ -7,8 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class CardRequestPosts extends Model
 {
     protected $table = "cards_request_posts";
-    protected $fillable = ['card_request_id', 'card_object_id', 'user_id', 'created_at', 'updated_at',
-        'post', 'initial_card'];
+    protected $fillable = ['card_request_id', 'card_object_id', 'user_id', 'created_at', 'updated_at', 'post', 'initial_card'];
     
     public function getFields() {
         return $this->fillable;
