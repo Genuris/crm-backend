8363c516593c80f83306680188072058737e4f31
pm_225 <ka@lindenvalley.de>
2019-10-16 20:26:57 +0300

    fix

---
+++ database/migrations/2019_10_16_163728_change_cards_column_total_area.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Doctrine\DBAL\Types\FloatType;
+use Doctrine\DBAL\Types\Type;
+
+class ChangeCardsColumnTotalArea extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Type::hasType('double')) {
+            Type::addType('double', FloatType::class);
+        }
+        Schema::table('cards', function(Blueprint $table) {
+            $table->double('total_area')->change()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cards', function (Blueprint $table) {
+            $table->string('total_area')->nullable()->change();
+        });
+    }
+}
