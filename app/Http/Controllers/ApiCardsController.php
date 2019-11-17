d2b04d4136268d89b01e1049a3af3ff168eff845
pm_225 <ka@lindenvalley.de>
2019-11-17 21:47:22 +0200

    add new fields

--- app/Http/Controllers/ApiCardsController.php
+++ app/Http/Controllers/ApiCardsController.php
@@ -1689,18 +1689,29 @@ class ApiCardsController extends Controller
         $card_request_status = CardRequestStatus::where('card_request_id', '=', $card_request_id)
             ->where('card_object_id','=', $card_object_id)->first();
         if ($card_request_status) {
-            $card_request_status->status = $status;
+            if (isset($status)) {
+                $card_request_status->status = ($status === '' ? null : $status);
+            }
+            if (isset($show_time)) {
+                $card_request_status->show_time = ($show_time === '' ? null : (int)$show_time);
+            }
             $card_request_status->user_id = $this->current_user_id;
-            $card_request_status->show_time = (int)$show_time;
             $card_request_status->save();
         } else {
             $data = [
-                'status' => $status,
+                //'status' => ($status === '' ? null : $status),
                 'card_request_id' => $card_request_id,
                 'card_object_id' => $card_object_id,
                 'user_id' => $this->current_user_id,
-                'show_time' => (int)$show_time,
+                //'show_time' => ($show_time === '' ? null : (int)$show_time),
             ];
+            if (isset($status)) {
+                $data['status'] = ($status === '' ? null : $status);
+            }
+            if (isset($show_time)) {
+                $data['show_time'] = ($show_time === '' ? null : (int)$show_time);
+            }
+
             $card_request_status = CardRequestStatus::create($data);
 
         }
